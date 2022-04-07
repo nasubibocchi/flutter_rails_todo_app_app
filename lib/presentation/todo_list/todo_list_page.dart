f54ad1a25ac226c46f3856ea69a98885e1cf551e
nasubibocchi <matsuhito125@gmail.com>
2022-04-07 18:14:26 +0900

    Add case that list is empty

--- lib/presentation/todo_list/todo_list_page.dart
+++ lib/presentation/todo_list/todo_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rails_todo_app/presentation/add_todo_page/add_todo_page.dart';
 import 'package:flutter_rails_todo_app/presentation/todo_list/todo_list_view_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -12,34 +13,43 @@ class TodoListPage extends HookConsumerWidget {
     final state = ref.read(todoListViewModelProvider);
 
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => AddTodoPage()));
+          },
+          child: const Icon(Icons.add),
+        ),
         body: state.when(
-      (todoList) {
-        return ListView.builder(
-            itemCount: todoList.length,
-            itemBuilder: (context, int index) {
-              return Card(
-                child: Row(
-                  children: [
-                    const SizedBox(width: 10),
-                    Checkbox(
-                        value: todoList[index].isDone,
-                        onChanged: (_) {
-                          ref
-                              .read(todoListViewModelProvider.notifier)
-                              .onTapCheckBox();
-                          ref
-                              .read(todoListViewModelProvider.notifier)
-                              .onRefresh();
-                        }),
-                    const SizedBox(width: 20),
-                    Expanded(child: Text(todoList[index].body)),
-                  ],
-                ),
-              );
-            });
-      },
-      loading: () => const Center(child: CircularProgressIndicator()),
-      error: () => const SizedBox(),
-    ));
+          (todoList) {
+            return todoList.isEmpty
+                ? const SizedBox()
+                : ListView.builder(
+                    itemCount: todoList.length,
+                    itemBuilder: (context, int index) {
+                      return Card(
+                        child: Row(
+                          children: [
+                            const SizedBox(width: 10),
+                            Checkbox(
+                                value: todoList[index].isDone,
+                                onChanged: (_) {
+                                  ref
+                                      .read(todoListViewModelProvider.notifier)
+                                      .onTapCheckBox();
+                                  ref
+                                      .read(todoListViewModelProvider.notifier)
+                                      .onRefresh();
+                                }),
+                            const SizedBox(width: 20),
+                            Expanded(child: Text(todoList[index].body)),
+                          ],
+                        ),
+                      );
+                    });
+          },
+          loading: () => const Center(child: CircularProgressIndicator()),
+          error: () => const SizedBox(),
+        ));
   }
 }
